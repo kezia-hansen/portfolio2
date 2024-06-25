65a3eb2691a52e3523d72b8e5ff3bdb403a01bf6
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-06-25 18:17:50 +0200

    done for now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,11 @@ export default function Navbar() {
   return (
     <nav className="bg-orange-950 border-orange-950">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse hover:scale-110">
           <Image src="../logo.svg" width={100} height={100} className="h-16" alt="Logo" />
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-orange-100 bg-orange-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm px-4 py-2 text-center ">
+          <button type="button" className="text-orange-100 bg-orange-700 hover:bg-orange-500 hover:scale-110 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm px-4 py-2 text-center ">
             Contact me
           </button>
           <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-orange-100 rounded-lg md:hidden hover:bg-orange-700 focus:outline-none " aria-controls="navbar-cta" aria-expanded={isOpen} onClick={() => setIsOpen(!isOpen)}>
