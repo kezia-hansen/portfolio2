f3ec49ba48d70e059241ee62ad67d8427a519a83
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-06-25 15:37:26 +0200

    for now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
+
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="bg-orange-950 border-orange-950">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Image src="../logo.svg" width={100} height={100} className="h-16" alt="Flowbite Logo" />
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <Image src="../logo.svg" width={100} height={100} className="h-16" alt="Logo" />
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-orange-100 bg-orange-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Get started
+          <button type="button" className="text-orange-100 bg-orange-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">
+            Contact me
           </button>
-          <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded={isOpen} onClick={() => setIsOpen(!isOpen)}>
+          <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-orange-100 rounded-lg md:hidden hover:bg-orange-700 focus:outline-none " aria-controls="navbar-cta" aria-expanded={isOpen} onClick={() => setIsOpen(!isOpen)}>
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -22,25 +23,25 @@ export default function Navbar() {
           </button>
         </div>
         <div className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isOpen ? "block" : "hidden"}`} id="navbar-cta">
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
             <li>
-              <a href="#" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">
+              <a href="/" className="block py-2 px-3 md:p-0 text-orange-100 rounded   md:hover:bg-orange-700 hover:scale-105">
                 Home
               </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                About
+              <a href="#" className="block py-2 px-3 md:p-0 text-orange-100 rounded   md:hover:bg-orange-700 hover:scale-105 ">
+                About Me
               </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                Services
+              <a href="#" className="block py-2 px-3 md:p-0 text-orange-100 rounded   md:hover:bg-orange-700 hover:scale-105 ">
+                Skills
               </a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                Contact
+              <a href="#" className="block py-2 px-3 md:p-0 text-orange-100 rounded   md:hover:bg-orange-700 hover:scale-105 ">
+                Projects
               </a>
             </li>
           </ul>
